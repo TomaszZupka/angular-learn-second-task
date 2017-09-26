b1d2570849a74e80b440bd852a93abdfcb4f22ed
Tomasz Zupka <tomasz.zupka@gmail.com>
2017-09-26 12:09:11 +0200

    game control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  onInternalFired(firedNumber: number) {
+    console.log(firedNumber);
+  }
 }
